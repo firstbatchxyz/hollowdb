4dad4c12daf2537cf2b7aaac8184ac707f8eaa72
Erhan Tezcan <erhany96@gmail.com>
2023-03-08 19:01:02 +0300

    added Prover util class

---
+++ tests/hollow-authz.test.ts
@@ -0,0 +1,72 @@
+import {readFileSync} from 'fs';
+import {Prover} from '../src';
+const snarkjs = require('snarkjs');
+
+// WASM and prover key for generating proofs
+const WASM_PATH = './circuits/hollow-authz/hollow-authz.wasm';
+const PROVERKEY_PATH = './circuits/hollow-authz/prover_key.zkey';
+
+// verification key to verify the proofs
+const VERIFICATIONKEY_PATH = './circuits/hollow-authz/verification_key.json';
+
+const preimage = BigInt(1122334455);
+const curValue = {
+  lorem: 'ipsum',
+  foo: 123,
+  bar: true,
+};
+const newValue = {
+  lorem: 'dolor',
+  foo: 987,
+  bar: false,
+};
+
+describe('hollow-authz circuit', () => {
+  let prover: Prover;
+  let verificationKey: object;
+  let proof: object;
+  let correctKey: string;
+  let correctCurValueHash: string;
+  let correctNewValueHash: string;
+
+  beforeAll(async () => {
+    // prepare prover and verification key
+    prover = new Prover(WASM_PATH, PROVERKEY_PATH);
+    verificationKey = JSON.parse(readFileSync(VERIFICATIONKEY_PATH).toString());
+
+    // generate a proof
+    const fullProof = await prover.generateProof(preimage, curValue, newValue);
+    proof = fullProof.proof;
+    correctCurValueHash = fullProof.publicSignals[0];
+    correctNewValueHash = fullProof.publicSignals[1];
+    correctKey = fullProof.publicSignals[2];
+  });
+
+  it('should verify proof', async () => {
+    const result = await snarkjs.groth16.verify(
+      verificationKey,
+      [correctCurValueHash, correctNewValueHash, correctKey],
+      proof
+    );
+    expect(result).toEqual(true);
+  });
+
+  it('should NOT verify proof with wrong current value', async () => {
+    const result = await snarkjs.groth16.verify(verificationKey, ['12345', correctNewValueHash, correctKey], proof);
+    expect(result).toEqual(false);
+  });
+
+  it('should NOT verify proof with wrong new value', async () => {
+    const result = await snarkjs.groth16.verify(verificationKey, [correctCurValueHash, '12345', correctKey], proof);
+    expect(result).toEqual(false);
+  });
+
+  it('should NOT verify proof with wrong key', async () => {
+    const result = await snarkjs.groth16.verify(
+      verificationKey,
+      [correctCurValueHash, correctNewValueHash, '12345'],
+      proof
+    );
+    expect(result).toEqual(false);
+  });
+});
--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -4,11 +4,6 @@ import {Admin, SDK} from '../../src';
 import {CacheType} from '../../src/sdk/types';
 import {createClient} from 'redis';
 import {globals} from '../../jest.config.cjs';
-import {ripemd160} from '@ethersproject/sha2';
-const snarkjs = require('snarkjs');
-
-const WASM_PATH = './circuits/hollow-authz/hollow-authz.wasm';
-const PROVERKEY_PATH = './circuits/hollow-authz/prover_key.zkey';
 
 /**
  * Add funds to any wallet.
@@ -24,42 +19,6 @@ export async function addFunds(warp: Warp, wallet: JWKInterface) {
 export async function mineBlock(warp: Warp) {
   await warp.arweave.api.get('mine');
 }
-
-/**
- * Generate a proof for HollowDB.
- * If a value is given as null, it will be put as 0 in the proof.
- * @param preimage preimage of the key to be written at
- * @param curValue currently stored value
- * @param nextValue to be stored value
- * @returns a fullProof object with the proof and public signals
- */
-export async function generateProof(
-  preimage: bigint,
-  curValue: string | null,
-  nextValue: string | null
-): Promise<{proof: object; publicSignals: [curValueHash: bigint, nextValueHash: bigint, key: bigint]}> {
-  const fullProof = await snarkjs.groth16.fullProve(
-    {
-      preimage: preimage,
-      curValueHash: curValue ? valueToBigInt(curValue) : 0n,
-      nextValueHash: nextValue ? valueToBigInt(nextValue) : 0n,
-    },
-    WASM_PATH,
-    PROVERKEY_PATH
-  );
-  return fullProof;
-}
-
-/**
- * Convert a value into bigint using ripemd160.
- * - Ripemd160 outputs a hex string, which can be converted into a bigint.
- * - Since the result is 160 bits, it is for sure within the finite field of BN128.
- * @see https://docs.circom.io/background/background/#signals-of-a-circuit
- * @param value any kind of value
- */
-export const valueToBigInt = (value: string): bigint => {
-  return BigInt(ripemd160(Buffer.from(JSON.stringify(value))));
-};
 
 /**
  * Utility function to create `Admin` and `SDK`s for HollowDB contract.
