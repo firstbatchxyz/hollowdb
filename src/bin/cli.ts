269bd3ca76bc4b16c55fd388f3610d9a75aa8352
erhant <erhany96@gmail.com>
2023-11-14 22:29:44 +0300

    fix lint and test errors

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {copyFileSync, existsSync, readFileSync, writeFileSync} from 'fs';
+import {copyFileSync, readFileSync, writeFileSync} from 'fs';
 import yargs from 'yargs';
 import {hideBin} from 'yargs/helpers';
 
